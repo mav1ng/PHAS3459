e41c1d6a5e704fceaeb71d6fa90f3a370788da2f
zcapmvs <zcapmvs@CT00006693.ad.ucl.ac.uk>
2017-10-06 13:08:01 +0100

    Final Changes. Submit - ready. Except for decimals problem in ac.

--- PHAS3459/src/module1/AlgorithmControl.java
+++ PHAS3459/src/module1/AlgorithmControl.java
@@ -1,40 +1,44 @@
 package module1;
 
 public class AlgorithmControl {
-	
-	
-	
+
+
+
 	int i = 0;
 	int imax = 10;
-	
+
 	public void loop() {					//creation of method loop()
 		for ( i = 0; i <= imax; i++) {
 			System.out.println(i);
 		}
 	}	
-		
+
 	boolean stop = true;
 	int x = 5;
-		
+
 	public void decrement() {				//creation of method decrement()
 		while (stop) {
+			System.out.println(x);
 			x--;
 			stop = (x>(-12));
 		}
 	}
-	
+
 	boolean stop1 = true;
 	double y = 2.4;
-	
+
 	public void increment() {				//creation of method increment()
 		while (stop1) {
-			y = y+0.2;
-			stop1 = (y <= 8.8);
+			//String output = String.format("%.2f", y); trying to only print out 2 decimal places
+			//			System.out.printf("%.2d", y);
+			System.out.println(y);
+			y += 0.2;
+			stop1 = (y<=8.8);
 		}
-		
+
 	}
-	
-	
+
+
 	long timeNow = System.currentTimeMillis();
 	long intValB;
 	long intValA;
@@ -44,53 +48,62 @@ public class AlgorithmControl {
 	int loopSteps;
 	long maxTime;
 	boolean timervar = true;
-	
-	
-			
+
+
+
 	public int timer(long maxTime, int loopSteps) {		//creation of timer() method
-		
+
 		timeNow = System.currentTimeMillis();
 		intValB = timeNow;
-		
+
 		while (timervar) {
-			
+
 			long timeNow = System.currentTimeMillis();
 			intValA = timeNow;
 			iremainder = (int) (intValA % intValB);
 			timervar = (iremainder<=maxTime);
 			w++;
-			
+
 			if (w==loopSteps) {
 				w = 0;
 				totalLoops = totalLoops + loopSteps;
 				System.out.println(totalLoops);	
 			}
-			
+
 		}
-		
+
 		iremainder = 0;
 		timervar = true;
-		
+
 		return totalLoops;
 	}
-														//finished creation of timer() method
+	//finished creation of timer() method
 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		AlgorithmControl ac = new AlgorithmControl();
-		
+
+		ac.loop();
+		ac.decrement();
+		ac.increment();
+
+		System.out.println(" ");
+		System.out.println(" ");
+
 		int result1 = ac.timer(8000, 1000);
 		System.out.println("Total number of loops: " + result1); 	//first run
-		
+
 		int result2 = ac.timer(8000, 40000);
 		System.out.println("Total number of loops: " + result2);	//second run
 		System.out.println(" ");
-		
+
 		System.out.println("First go: " + result1 + " Second go: " + result2);	//printing out the results
-		
+
 		System.out.println("The number of total loops of the second go is significantly larger then the one of the first go. The reason for that is the time the system needs to print out a line. Since, in the first go the System has to print out every 1000th loop, which is a lot more than every 40000th, it needs a lot more time. Since the maxTime is the same for both runs the total number of loops is significantly different.");
+
+			
 	}
 
 }
--- PHAS3459/src/module1/VectorMethods.java
+++ PHAS3459/src/module1/VectorMethods.java
@@ -5,21 +5,29 @@ public class VectorMethods {
 
 	//defining the function dotProduct;
 	public double dotProduct (double a, double b, double c, double d, double e, double f) {
+		
 		double dotP;
 		dotP = a*d+b*e+c*f;
+		
 		return dotP;
 	}
 	
+	
 	//defining the method magnitude
 	public double magnitude (double x, double y, double z) {
+		
 		double magvar = Math.sqrt(x*x + y*y + z*z);
+		
 		return magvar;
 	}
 	
+	
 	//defining the method angle
 	public double angle (double a, double b, double c, double d, double e, double f) {
+		
 		double anglevar;
 		anglevar = (dotProduct(a,b,c,d,e,f))/(magnitude(a,b,c)*magnitude(d,e,f));
+		
 		return anglevar;
 	}
 	
@@ -28,14 +36,19 @@ public class VectorMethods {
 		
 		VectorMethods vm = new VectorMethods();
 		
+		
+		
 		//calculation of the angle between the two vectors (2,3,4) and (1,3,2)
 		double resvar = vm.angle(2,3,4,1,3,2);
 		resvar = Math.acos(resvar);
+		
 		System.out.println("The angle between the two vectors is "+resvar*(360/(2*Math.PI)));
+		
 		
 		//calculation of the angle between the two vectors (2,3,4) and (0,0,0)
 		double resvar2 = vm.angle(2,3,4,0,0,0);
 		resvar2 = Math.acos(resvar2);
+		
 		System.out.println("The angle between the two vectors is " + resvar2 +" /not defined");
 		System.out.println("Since you cannot divide by 0");
 
