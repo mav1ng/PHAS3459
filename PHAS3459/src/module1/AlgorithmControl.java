51ebbb44e7f3d0796569d3b2ec54b5db4271ca1a
zcapmvs <zcapmvs@CT00006693.ad.ucl.ac.uk>
2017-10-06 12:17:34 +0100

    Added comments

--- PHAS3459/src/module1/AlgorithmControl.java
+++ PHAS3459/src/module1/AlgorithmControl.java
@@ -7,7 +7,7 @@ public class AlgorithmControl {
 	int i = 0;
 	int imax = 10;
 	
-	public void loop() {
+	public void loop() {					//creation of method loop()
 		for ( i = 0; i <= imax; i++) {
 			System.out.println(i);
 		}
@@ -16,7 +16,7 @@ public class AlgorithmControl {
 	boolean stop = true;
 	int x = 5;
 		
-	public void decrement() {
+	public void decrement() {				//creation of method decrement()
 		while (stop) {
 			x--;
 			stop = (x>(-12));
@@ -26,7 +26,7 @@ public class AlgorithmControl {
 	boolean stop1 = true;
 	double y = 2.4;
 	
-	public void increment() {
+	public void increment() {				//creation of method increment()
 		while (stop1) {
 			y = y+0.2;
 			stop1 = (y <= 8.8);
@@ -39,13 +39,15 @@ public class AlgorithmControl {
 	long intValB;
 	long intValA;
 	int w = 0;
-	int iremainder;
+	int iremainder;									//declaration of variables for timer() method
 	int totalLoops;
 	int loopSteps;
 	long maxTime;
 	boolean timervar = true;
+	
+	
 			
-	public int timer(long maxTime, int loopSteps) {
+	public int timer(long maxTime, int loopSteps) {		//creation of timer() method
 		
 		timeNow = System.currentTimeMillis();
 		intValB = timeNow;
@@ -71,7 +73,7 @@ public class AlgorithmControl {
 		
 		return totalLoops;
 	}
-
+														//finished creation of timer() method
 
 
 	public static void main(String[] args) {
@@ -80,13 +82,13 @@ public class AlgorithmControl {
 		AlgorithmControl ac = new AlgorithmControl();
 		
 		int result1 = ac.timer(8000, 1000);
-		System.out.println("Total number of loops: " + result1);
+		System.out.println("Total number of loops: " + result1); 	//first run
 		
 		int result2 = ac.timer(8000, 40000);
-		System.out.println("Total number of loops: " + result2);
+		System.out.println("Total number of loops: " + result2);	//second run
 		System.out.println(" ");
 		
-		System.out.println("First go: " + result1 + " Second go: " + result2);
+		System.out.println("First go: " + result1 + " Second go: " + result2);	//printing out the results
 		
 		System.out.println("The number of total loops of the second go is significantly larger then the one of the first go. The reason for that is the time the system needs to print out a line. Since, in the first go the System has to print out every 1000th loop, which is a lot more than every 40000th, it needs a lot more time. Since the maxTime is the same for both runs the total number of loops is significantly different.");
 	}
