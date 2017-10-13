e79117efae5bc1bf449ec471bd672eaa818b6d50
Max Viktor Spreng <max.spreng.17@ucl.ac.uk>
2017-10-13 13:28:27 +0100

    commented all the calculations

--- PHAS3459/src/module2/FallingParticle.java
+++ PHAS3459/src/module2/FallingParticle.java
@@ -2,6 +2,9 @@ package module2;
 
 public class FallingParticle {
 	
+	//declaring the used variables
+	//m=mass, d=drag coefficient, t=elapsed time, z=position
+	//h=initial height, v=current velocity, g=earth gravity constant
 	final double m;
 	double d;
 	static double t;
@@ -23,6 +26,8 @@ public class FallingParticle {
 	
 	//method to get the current position of the particle
 	public double getZ() {
+		//calculation of the current position of the object where t is the time elapsed
+		//and g is the gravity constant and h is the initial height
 		this.z = -(1/2)*g*t*t + h;
 		return this.z;
 	}
@@ -34,6 +39,7 @@ public class FallingParticle {
 	
 	//method to get the velocity of the particle
 	public double getV() {
+		//calculating the velocity after an elapsed time of t
 		this.v = g * t + this.v;
 		return this.v;
 	}
@@ -47,7 +53,9 @@ public class FallingParticle {
 	public void doTimeStep(double deltaT) {
 		//calculating the current acceleration of the particle
 		double a = ((this.d*this.v*this.v)/m)-g;
+		//updating the current speed
 		this.v = this.v + a*deltaT;
+		//updating the current position
 		this.z = this.z + (this.v*deltaT);
 	}
 	
