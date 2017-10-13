2bbef739ee0b3ad9724101b732da26de5b289c89
Max Viktor Spreng <max.spreng.17@ucl.ac.uk>
2017-10-13 12:25:46 +0100

    created class ParticleMain, created object ball, set height

---
+++ PHAS3459/src/module2/ParticleMain.java
@@ -0,0 +1,20 @@
+package module2;
+
+public class ParticleMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//creating object ball from the class FallingParticle
+		FallingParticle ball = new FallingParticle(5.2, 3.6);
+		
+		//set the initial height to 10m above the floor
+		ball.setH(10);
+		
+		//simulation the drop and printing out the time taken and the velocities for 
+		//different timesteps deltaT
+		
+		
+	}
+
+}
