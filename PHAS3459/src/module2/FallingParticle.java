a3008c115c71702ab5f03a98d2a12235791b785d
Max Viktor Spreng <max.spreng.17@ucl.ac.uk>
2017-10-13 12:06:28 +0100

    added the getZ() method of the particle

--- PHAS3459/src/module2/FallingParticle.java
+++ PHAS3459/src/module2/FallingParticle.java
@@ -1,6 +1,25 @@
 package module2;
 
 public class FallingParticle {
+	
+	final double m;
+	double d;
+	double t;
+	double z;
+	double h;
+	double v;
+	final double g = 9.81;
+	
+	//constructor
+	public FallingParticle(double m, double d) {
+		this.m = m;
+		this.d = d;
+	}
+	
+	//method to set the height of the falling particle
+	public void setH(double h) {
+		this.h = h;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
