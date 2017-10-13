25fbd66bad457cd0c3aac543a6240fbd116e5509
Max Viktor Spreng <max.spreng.17@ucl.ac.uk>
2017-10-13 10:21:10 +0100

    added the class complex and the constructor

---
+++ PHAS3459/src/module2/Complex.java
@@ -0,0 +1,18 @@
+package module2;
+
+public class Complex {
+	
+	double x;
+	double y;
+	
+	public Complex(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
