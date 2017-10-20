0b0c266042592ee624850a0e0c2c16743492d947
Max Viktor Spreng <max.spreng.17@ucl.ac.uk>
2017-10-20 12:34:11 +0100

    added to Alphabet: for-loop, isLetterisDigit, parseInt, tryBlock, printed out

---
+++ PHAS3459/src/module3/Alphabet.java
@@ -0,0 +1,54 @@
+package module3;
+
+import java.util.* ;
+
+public class Alphabet {
+
+	static char randomCharacter() {
+		//creating new random number generator
+		Random Rvar = new Random();
+		//creates random int between 0 and 127
+		int randomint = Rvar.nextInt(128);
+		//converting in to type char
+		char rc = (char)(randomint);
+		return rc;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int runningTotal = 0;
+		int excCount = 0;
+		
+		java.lang.StringBuilder str = new java.lang.StringBuilder();
+		
+		for (int i = 0; i <= 250; i++) {
+			char randomchar = randomCharacter();
+			//testing if randomchar is a Letter or a Digit
+			if (Character.isLetter(randomchar) || Character.isDigit(randomchar)) {
+				str.append(randomchar);
+				//if randomchar is a number
+				try {
+					int runningVar = Integer.parseInt(Character.toString(randomchar));
+					runningTotal = runningTotal + runningVar;
+				}
+				//if parseInt throws an exception (randomchar is a letter)
+				catch (Exception e) {
+					System.out.println(e);
+					excCount++;
+				}
+				
+			}
+		}
+		
+		System.out.println("Contents of the StringBuilder: str ");
+		System.out.println(str);
+		System.out.println("Lenght of str: " + str.length());
+		System.out.println("Sum of all the Numbers: " + runningTotal);
+		System.out.println("Number of all exceptions " + excCount);
+		
+	}
+
+}
