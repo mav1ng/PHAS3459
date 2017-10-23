a5a9fa948822d424166112983191b969c9773fb5
Viktor <Viktor@10.200.132.57>
2017-10-23 20:16:45 +0100

    added analysisStart method not sure about the variables

--- PHAS3459/src/module4/NumericalReader.java
+++ PHAS3459/src/module4/NumericalReader.java
@@ -1,8 +1,11 @@
 package module4;
 
 import java.io.*;
+import java.net.URL;
 
 public class NumericalReader {
+	
+	private double minValue, maxValue, nValues, sumOfValues;
 	
 	//defining the method that promps user to input string and then stores it
 	public static String getStringFromKeyboard() throws Exception {
@@ -23,19 +26,38 @@ public class NumericalReader {
 		
 		}
 	}
+	
+	public BufferedReader brFromURL(String urlName) throws Exception {
+		try {
+			//creating a new URL object it will throw an exception when the entered urlName is not a URL
+			URL website = new URL(urlName);
+			//creating the BufferedReader object 
+			BufferedReader in = new BufferedReader(new InputStreamReader(website.openStream()));
+			return in;
+		}
+		catch (Exception e) {
+			throw (e);
+		}	
+	}
+	
+	void analysisStart(String dataFile) throws Exception {
 		
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+		try {
+		
+		File inputFile = new File(dataFile);
+		
+		//initializing the private variables
+		minValue = 0;
+		maxValue = 0;
+		nValues = 0;
+		sumOfValues = 0;
+
+		}
+		catch (Exception e) {
+			throw (e);
+		}
+	}
+		
 	
 	
 	
