553fd11d087dfa04bda8051554f622b278fc37de
Max Viktor Spreng <max.spreng.17@ucl.ac.uk>
2017-10-23 15:26:56 +0100

    changed brFromURL and brFromFile so that it acutally reads the contents

--- PHAS3459/src/module4/WordCounter.java
+++ PHAS3459/src/module4/WordCounter.java
@@ -2,17 +2,21 @@ package module4;
 
 //importing the Class BufferedReader into module4
 import java.io.* ;
+//importing the Class java.net
+import java.net.*;
 
 public class WordCounter {
 
 	
 	
 	//method brFromURL that takes the URL of a 
-	//website as an input and returns it as a BufferedReader object or throws an exception
+	//website as an input and returns its contents
+	//as a BufferedReader object or throws an exception
 	public static BufferedReader brFromURL(String urlName) throws Exception {
-		
+	
 		try {
-			BufferedReader in = new BufferedReader(new FileReader(urlName));
+			URL website = new URL(urlName);
+			BufferedReader in = new BufferedReader(new InputStreamReader(website.openStream()));
 			return in;
 		}
 		catch (Exception e) {
@@ -26,11 +30,19 @@ public class WordCounter {
 	public static BufferedReader brFromFile(String fileName) throws Exception {
 		
 		try {
-			BufferedReader in = new BufferedReader(new FileReader(fileName));
+			BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
 			return in;
+			
 		}
 		catch (Exception e) {
 			throw (e);
+		}
+	}
+	
+	
+	public static int countWordsinResource(BufferedReader dataAsBR) throws Exception {
+		try {
+			
 		}
 	}
 
