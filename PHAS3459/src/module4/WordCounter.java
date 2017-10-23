0a9a180d7bb54be9df417e027f135e54660f9e49
Max Viktor Spreng <max.spreng.17@ucl.ac.uk>
2017-10-23 14:54:13 +0100

    WordCounter: added brFromFile method

--- PHAS3459/src/module4/WordCounter.java
+++ PHAS3459/src/module4/WordCounter.java
@@ -5,12 +5,28 @@ import java.io.* ;
 
 public class WordCounter {
 
-	//method BufferedReader that takes the URL of a 
-	//website as an input and returns it as a BufferedReader Object
+	
+	
+	//method brFromURL that takes the URL of a 
+	//website as an input and returns it as a BufferedReader object or throws an exception
 	public static BufferedReader brFromURL(String urlName) throws Exception {
 		
 		try {
 			BufferedReader in = new BufferedReader(new FileReader(urlName));
+			return in;
+		}
+		catch (Exception e) {
+			throw (e);
+		}
+	}
+	
+	
+	//method brFromFile that takes the fileName as a String input and returns it
+	//as a BufferedReader object or throws an exception
+	public static BufferedReader brFromFile(String fileName) throws Exception {
+		
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(fileName));
 			return in;
 		}
 		catch (Exception e) {
