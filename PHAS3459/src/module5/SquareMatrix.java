b9c902fa05981d9a4bd04bff5785d4eea5942df2
Max Viktor Spreng <max.spreng.17@ucl.ac.uk>
2017-10-30 15:34:33 +0000

    added the try catch block to the constructor

--- PHAS3459/src/module5/SquareMatrix.java
+++ PHAS3459/src/module5/SquareMatrix.java
@@ -5,17 +5,26 @@ public class SquareMatrix {
 	
 	//Constructor 
 	public SquareMatrix(double[][] elements) throws Exception {
-		int length1 = elements[0].length;
-		int length2 = elements[1].length;
-		double [][] sqMatrix = new double[length1][length1];
 		
-		//creating the matrix Object and assigning the values from the elements matrix
-		//should throw an Exception if length1 != length2
-		for (int i = 0; i <= length1; i++) {
-			for (int k = 0; k <= length2; k++ ) {
-				sqMatrix[i][k] = elements[i][k];
+		try {
+			int length1 = elements[0].length;
+			int length2 = elements[1].length;
+			double [][] sqMatrix = new double[length1][length1];
+			
+			//creating the matrix Object and assigning the values from the elements matrix
+			//should throw an Exception if length1 != length2
+			for (int i = 0; i <= length1; i++) {
+				for (int k = 0; k <= length2; k++ ) {
+					sqMatrix[i][k] = elements[i][k];
+				}
 			}
+			
 		}
+		catch (Exception e) {
+			System.out.println("The entered array is not a square matrix.");
+			throw e;
+		}
+		
 	}
 
 }
