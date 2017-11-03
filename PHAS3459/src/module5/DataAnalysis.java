085c6163312c48ff3b3d9145443f6ea1bf83dfce
Viktor <Viktor@10.200.131.201>
2017-11-03 21:08:20 +0000

    added comments to the dataFromURL() method

--- PHAS3459/src/module5/DataAnalysis.java
+++ PHAS3459/src/module5/DataAnalysis.java
@@ -10,23 +10,31 @@ public class DataAnalysis {
 		
 		//creating the new URL
 		URL inWebsite = new URL(url);
+		
 		//opening the input stream from the URL
 		InputStream inStream = inWebsite.openStream();
+		
 		//decodes the input stream into characters
 		InputStreamReader inReader = new InputStreamReader(inStream);
+		
 		//buffering the input stream 
 		BufferedReader br = new BufferedReader(inReader);
+		
 		
 		//creating a new ArrayList object that is supposed to get DataPoint objects
 		ArrayList<DataPoint> al = new ArrayList<DataPoint>();
 		
 		String line;
 		
+		//filling in the array list
 		try {
 			while ((line = br.readLine()) != null) {
+				//calling on the createFromURL method to scann trough the line and create the 
+				//DataPoint object which then gets added to the ArrayList "al" which holds
+				//objects from the type DataPoints
 				al.add(DataPoint.createFromURL(line));
 			}
-			
+		//returning the Arraylist	
 		return	al;		
 		
 		} 
