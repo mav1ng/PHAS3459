06cba5dc726296ef2a0e310a72a96cd2b2dbf72b
Max Viktor Spreng <max.spreng.17@ucl.ac.uk>
2017-11-03 10:31:26 +0000

    created the main method with the first sum

--- PHAS3459/src/module5/TestSquareMatrix.java
+++ PHAS3459/src/module5/TestSquareMatrix.java
@@ -1,5 +1,39 @@
 package module5;
 
+
 public class TestSquareMatrix {
 
+	public static void main(String[] args) {
+		
+		//defining the arrays that describe the matrices
+		double[][] arrayA = {{1, 2, 0}, {0, 2, 0 }, {-2, 0, 1}};
+		double[][] arrayB = {{2, 1, 0}, {0, 1, 0 }, {-1, 0, 1}};
+		double[][] arrayC = {{4, 2}, {3, 2}};
+		double[][] arrayD = {{-2, 3}, {3, -4}};
+		double[][] arrayI = {{1, 0}, {0, 1}};
+		
+		//creating the SqareMatrix objects
+		try {
+			SquareMatrix A = new SquareMatrix(arrayA);
+			SquareMatrix B = new SquareMatrix(arrayB);
+			SquareMatrix C = new SquareMatrix(arrayC);
+			SquareMatrix D = new SquareMatrix(arrayD);
+			SquareMatrix I = new SquareMatrix(arrayI);
+			
+			
+			System.out.println("Printing the the sum A+B:");
+			System.out.println();
+			
+			System.out.println(SquareMatrix.add(A, B));
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+
+		
+		
+	}
+		
 }
+	
+
