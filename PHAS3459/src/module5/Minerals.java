9c46058a5bdd753798acadccb95508f0050c51b9
Viktor <Viktor@10.200.131.70>
2017-11-04 16:45:51 +0000

    finished class minerals

--- PHAS3459/src/module5/Minerals.java
+++ PHAS3459/src/module5/Minerals.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -13,13 +14,13 @@ import java.net.URL;
 public class Minerals {
 	
 	//creating the HashMap for the minerals with its mass
-	HashMap<Double, Double> codeMass = new HashMap<Double, Double>();
+	static HashMap<Double, Double> codeMass = new HashMap<Double, Double>();
 	//Location and Code of the minerals
-	HashMap<Double, String> locCode = new HashMap<Double, String>();
+	static HashMap<Double, String> locCode = new HashMap<Double, String>();
 	
 	
 	//method that gets the information from the website and fills the information into the HashMap
-	public HashMap<Double, Double> getDataFromURL(String url) throws Exception {
+	public static HashMap<Double, Double> getDataFromURL(String url) throws Exception {
 		
 		//creating the BuffereReader from with the Stream from the URL
 		URL inWebsite1 = new URL(url);
@@ -33,7 +34,7 @@ public class Minerals {
 		while ((line = br.readLine()) != null) {
 			Scanner scanner = new Scanner(line);
 			Scanner sc = scanner.useLocale(Locale.ENGLISH);
-			this.codeMass.put(sc.nextDouble(), sc.nextDouble());
+			codeMass.put(sc.nextDouble(), sc.nextDouble());
 			scanner.close();
 		}
 		
@@ -47,7 +48,7 @@ public class Minerals {
 	}
 	
 	//method that gets the information from the website and fills in the information into the second HashMap
-	public HashMap<Double, String> getDataFromURL2(String url) throws Exception {
+	public static HashMap<Double, String> getDataFromURL2(String url) throws Exception {
 		
 		//creating the BuffereReader from with the Stream from the URL
 		URL inWebsite1 = new URL(url);
@@ -63,7 +64,7 @@ public class Minerals {
 			Scanner scanner = new Scanner(line);
 			Scanner sc = scanner.useLocale(Locale.ENGLISH);
 			location = sc.next();
-			this.locCode.put(sc.nextDouble(), location);
+			locCode.put(sc.nextDouble(), location);
 			scanner.close();
 		}
 		
@@ -76,17 +77,22 @@ public class Minerals {
 		
 	}
 	
+	//method to compare and print out the maximum Mass and according code and location and 
+	//same with the minimum Mass
 	public static void analyseData(HashMap<Double, Double> hpMass, 
 			HashMap<Double, String> hmLoc) throws Exception {
 		
 		//creating a collection with the different mass values of the minerals
 		Collection<Double> massValues = hpMass.values();
+		//creating a iteration with the different mass values of the minerals
 		Iterator<Double> mV = massValues.iterator();
 		
+		//declaring the needed variables
 		double maxMass = 0;
 		double minMass = 0;
 		double nextMass = 0;
 		
+		//finding the maximum and the minimum mass in the iteration
 		while (mV.hasNext()) {
 			if (maxMass < (nextMass = mV.next())) {
 				maxMass = nextMass;
@@ -96,11 +102,43 @@ public class Minerals {
 			}
 		}
 		
+		//defining the needed variables 
+		double maxCode = 0, minCode = 0;
+		String maxLoc = "", minLoc = "";
+
+		//getting the code for the mineral with the maximum mass and minimum mass
+		if (codeMass.containsValue(maxMass)) {
+			for (Map.Entry<Double, Double> entry : codeMass.entrySet()) {
+				if (entry.getValue() == maxMass) {
+					maxCode = entry.getKey();
+				}
+				if (entry.getValue() == minMass) {
+					minCode = entry.getKey();
+				}
+			}
+			
+			//getting the location for the mineral with the maximum mass and minimum mass
+			for(Map.Entry<Double, String> entry2 : locCode.entrySet()) {
+				if (entry2.getKey() == maxCode) {
+					maxLoc = entry2.getValue();
+				}
+				if (entry2.getKey() == minCode) {
+					minLoc = entry2.getValue();
+				}
+			}
+			
+		}
 		
+		//printing out the results
+		System.out.println("The mineral with the maximum mass " + maxMass 
+								+ "g has the code " + (int)(maxCode) +".");
 		
+		System.out.println("It has been found in the location " + maxLoc + ".");
+		System.out.println();
+		System.out.println("The mineral with the minimum mass " + minMass
+								+ "g has the code " + (int)(minCode) +".");
 		
-		System.out.println("The maximum Mass is: " + maxMass);
-		System.out.println("The minimum Mass is: " + minMass);
+		System.out.println("It has been found in the location " + minLoc + ".");
 		
 		
 	}
@@ -108,20 +146,18 @@ public class Minerals {
 	
 	public static void main(String[] args) {
 		
-		Minerals min = new Minerals();
-		
+		//getting the data from the website with the getDataFromURL methods and analysing the data with
+		//the analyseData() method
 		try {
-			min.getDataFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module5/module5-samples.txt");
-			min.getDataFromURL2("http://www.hep.ucl.ac.uk/undergrad/3459/data/module5/module5-locations.txt");
-			analyseData(min.codeMass, min.locCode);
+			getDataFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module5/module5-samples.txt");
+			getDataFromURL2("http://www.hep.ucl.ac.uk/undergrad/3459/data/module5/module5-locations.txt");
+			analyseData(codeMass, locCode);
 		}
 		catch (Exception e) {
 			System.out.println(e);
 		}
 	}
-	
-	
-	
+		
 	
 
 }
