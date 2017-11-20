9771e954c5d24b897834db308c7d6828198c3cda
Max Viktor Spreng <max.spreng.17@ucl.ac.uk>
2017-11-20 16:42:31 +0000

    added comments to the GoodnessOfFitCalculator interface

--- PHAS3459/src/module6/GoodnessOfFitCalculator.java
+++ PHAS3459/src/module6/GoodnessOfFitCalculator.java
@@ -5,14 +5,19 @@ import java.util.Iterator;
 
 public interface GoodnessOfFitCalculator {
 	
+	//method that calculates the chiSquaredValue taking a certain
+	//function and the datalist as an input
 	public static double goodnessOfFit(Collection<DataPoint> data, Theory theo) {
 		
-		double chiSqTotal = 0;
-		double chiSqSingle;
-		double currentTheoValue;
+
+		double chiSqTotal = 0;		//chi squared which defines the goodness of the fit
+		double chiSqSingle;			//the single chi squared quantities
+		double currentTheoValue;	//the corresponding theoretical value
 		
+		//Creating the ITerator from the ArrayList
 		Iterator<DataPoint> li = data.iterator();
 		
+		//Looping over the List and calculating the single chisquared values
 		while (li.hasNext()) {
 			
 			DataPoint currentDP = li.next();
@@ -22,6 +27,7 @@ public interface GoodnessOfFitCalculator {
 			chiSqSingle = (Math.pow(currentDP.getY() - currentTheoValue, 2)) 
 					/ Math.pow(currentDP.getEY(), 2);
 			
+			//adding the single chisquared values to the total chisquared value
 			chiSqTotal = chiSqTotal + chiSqSingle;
 			
 		}
