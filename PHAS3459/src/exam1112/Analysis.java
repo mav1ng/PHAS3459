629a9705d32db1a1a6202b951b1d102e8f1d8dd5
Max Viktor Spreng <max.spreng.17@ucl.ac.uk>
2018-01-12 11:39:21 +0000

    started to work on exam1112

---
+++ PHAS3459/src/exam1112/Analysis.java
@@ -0,0 +1,51 @@
+package exam1112;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Analysis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			Map<String, String> regionNames = readRegions("http://www.hep.ucl.ac.uk/undergrad/3459/exam-data/2011-12/final/regions.txt");
+					System.out.println(regionNames);
+		}
+		catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	
+	private static Map<String, String> readRegions(String url) throws IOException {
+		Map<String, String> regions = new HashMap<String, String>();
+		BufferedReader br = brFromURL(url);
+		br.readLine(); //Skip header line
+		String line;
+		while ((line = br.readLine()) != null) { //read lines untin end of file
+			Scanner s = new Scanner(line);
+			s.useDelimiter(",");
+			String id = s.next();
+			String name = s.next();
+			regions.put(id, name);
+			s.close();
+		}
+		return regions;
+	}
+	
+	private static BufferedReader brFromURL(String url) throws IOException {
+		URL u = new URL(url);
+		InputStreamReader isr = new InputStreamReader(u.openStream());
+		return new BufferedReader(isr);
+	}
+}
